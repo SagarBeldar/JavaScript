83e454245fdd2ed485b75e119ca053e8eddb3d89
SagarBeldar <sagarbeldar03@gmail.com>
2024-01-20 00:24:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+console.log("hello world");
+a=10;
+a=a+1;
+console.log(a);
+price=99,999;
+b="hello world";
+console.log(price);
+
+isFollow=true;
+
+//Function
+const student =()=>{
+    fullName=Sagar,
+    age=24,
+    cgpa=6.61,
+    isStudent=true
+}
+
+
+//Function
+const student2 =()=>{
+    fullName:aakash;
+    age:24;
+    cgpa:6.61;
+    isStudent:true
+}
+
+//This is object (key:value)
+const student1 ={
+        fullName:"Yash",
+        age:24,
+        cgpa:6.61,
+        isStudent:true,
+    
+}
+//accessing key valure of object
+console.log(student1.fullName);
+console.log(student1.isStudent);
+console.log(student1["age"]);
+console.log(student1.cgpa);
+
+//for incre. value of key
+
+student1.age=student1.age+1;
+console.log(student1.age);
+student1["age"]=student1["age"]+1;
+console.log(student1.age);
+
+const Profile={
+    fullName:"Sagar Beldar",
+    follow:true,
+    Posts:195,
+    follower:4,
+    Description:"For Education related account"
+}
+
+console.log(Profile);
+
